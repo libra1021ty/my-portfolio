d7ec3f75a8a76874099e0292caaf4ba07a54ea53
libra1021ty <libra1021ty424306111021@gmail.com>
2026-02-25 14:41:18 +0900

    4nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -130,6 +130,31 @@ function App() {
                 </div>
               </div>
             </div>
+            <div className='md:w-1/3 p-4'>
+              <div className='bg-gray-100 rounded-lg p-8'>
+                <div className='flex items-center mb-3'>
+                  <div className='bg-green-500 text-white rounded-full'>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M3 3H11V11H3V3Z" fill="currentColor" />
+                      <path d="M3 13H11V21H3V13Z" fill="currentColor" />
+                      <path d="M13 3H21V11H13V3Z" fill="currentColor" />
+                      <path d="M13 13H21V21H13V13Z" fill="currentColor" />
+                    </svg>
+                  </div>
+                  <h2 className='text-gray-900 text-lg font-medium ml-2'>SNS Chat App</h2>
+                </div>
+                <div>
+                  <p>ReactとJavaScriptで投稿機能付きのChatアプリ。仕様はReact, JavaScript, html, css, firebase, AWS, ログイン・ログアウト認証機能、メッセージ投稿機能、検索機能、お気に入り登録機能を実装しています。</p>
+                  <a href="#" className='mt-3 text-green-500 items-center'>もっとみる</a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
